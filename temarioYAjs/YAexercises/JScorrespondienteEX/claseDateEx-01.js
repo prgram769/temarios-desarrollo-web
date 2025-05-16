58d23b3c213a738bc8788982fe8001725530c391
prgram769 <prgram02@gmail.com>
2025-05-16 22:28:34 +0200

    Tercer commit, ejercicio 1 jsYa clase Date

--- temarioYAjs/YAexercises/JScorrespondienteEX/claseDateEx-01.js
+++ temarioYAjs/YAexercises/JScorrespondienteEX/claseDateEx-01.js
@@ -1,2 +1,49 @@
 // Confeccionar un programa que muestre en que cuatrimestre del año nos encontramos. Para esto obtener el mes. 
 
+let mesActual
+
+mesActual = prompt ("Dime el número del mes en el que nos encontramos:")
+mesActual = parseInt(mesActual)
+
+function averiguarCuatrimestre() {
+    switch (mesActual) {
+        case 1:
+            document.writeln("Primero")
+            break;
+        case 2:
+            document.writeln("Primero")
+            break
+        case 3:
+            document.writeln("Primero")
+            break
+        case 4:
+            document.writeln("Primero")
+            break
+        case 5:
+            document.writeln("Segundo")
+            break
+        case 6:
+            document.writeln("Segundo")
+            break
+        case 7:
+            document.writeln("Segundo")
+            break
+        case 8:
+            document.writeln("Segundo")
+            break
+        case 9:
+            document.writeln("Tercero")
+            break
+        case 10:
+            document.writeln("Tercero")
+            break
+        case 11:
+            document.writeln("Tercero")
+            break
+        case 12:
+            document.writeln("Tercero")
+            break;
+    }
+}
+
+averiguarCuatrimestre()
