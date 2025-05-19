464f258065d5d7be28abdc80b6391efd40a73d41
prgram769 <prgram02@gmail.com>
2025-05-19 20:04:10 +0200

    ejercicio claseArray resuelto

--- temarioYAjs/YAexercises/JScorrespondienteEX/claseArrayEx-01.js
+++ temarioYAjs/YAexercises/JScorrespondienteEX/claseArrayEx-01.js
@@ -3,16 +3,32 @@
 // El valor acumulado de los elementos del vector que sean mayores a 36.
 // Cantidad de valores mayores a 50.
 
-function mostrarValorAcumulado(elementos) {
-    for (let i = 0; i < elementos.lenght; i++) {
-        let v = prompt("Ingresa un elemento: ")
-        elementos[i] = parseInt(v)
+let vector = new Array(8)
+let total = 0
+
+for (let i = 0; i < vector.length; i++) {
+    let input = prompt("Ingrese un numero: ")
+    numero = parseInt(input)
+    vector[i] = numero
+    total += numero
+}
+
+let sumaE = 0
+
+for (const element of vector) {
+    if (element >= 36) {
+        sumaE += element
     }
 }
 
-function mostrarValoresMayor50(elementos) {
-    for (let i = 0; i < elementos.length; i++) {
-        
-        
+let contador = 0
+
+for (const element of vector) {
+    if (element >= 50) {
+        contador++
     }
 }
+
+document.writeln("Suma total de los numeros introducidos es: " + total + "<br>")
+document.writeln("La suma de los elementos mayores a 36 son: " + sumaE + "<br>")
+document.writeln("La cantidad de elementos mayores a 50 es: " + contador)
