200c84858e31926be1ed126d8f421924038959d5
prgram769 <prgram02@gmail.com>
2025-05-27 10:35:15 +0200

    nuevas clases y ejercicio

--- temarioJS/DOM/ejercicios/jscorrespondiente/practicaModificacionElementos.js
+++ temarioJS/DOM/ejercicios/jscorrespondiente/practicaModificacionElementos.js
@@ -1,1 +1,29 @@
-// 
+let imgA = document.getElementsByClassName("a")
+
+console.log(imgA)
+
+for (const img of imgA) {
+    img.style.border = "5px solid #00F"
+}
+
+let imgB = document.getElementsByClassName("b")
+
+console.log(imgB)
+
+Array.from(imgB).forEach(element => {
+    element.style.border = "5px solid black" 
+});
+
+let imgC = document.querySelector(".c")
+
+console.log(imgC);
+
+imgC.style.border = "5px solid #0F0"
+
+let imgD = document.querySelectorAll(".d")
+
+console.log(imgD);
+
+imgD.forEach(element => {
+    element.style.border = "5px solid #F00"
+});
