20de11ffaa48ec95ad78786b920980d815ee4825
prgram769 <prgram02@gmail.com>
2025-06-01 10:09:25 +0200

    nuevo ejercicio de formularios y eventos realizado

---
+++ temarioYAjs/YAexercises/JScorrespondienteEX/formulariosEventosEx-1.js
@@ -0,0 +1,13 @@
+// Crear un formulario con tres botones con las leyendas "1", "2" y "3". Mostrar un mensaje indicando qué botón se presionó. 
+
+function clickB1() {
+    alert("Has hecho click en el boton 1")
+}
+
+function clickB2() {
+    alert("Has hecho click en el boton 2")
+}
+
+function clickB3() {
+    alert("Has hecho click en el boton 3")
+}
